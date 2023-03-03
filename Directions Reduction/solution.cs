a30690c488ba2229e7f6a1c70e824173f3ba23dc
Bartosz Wieczorek <github@bartoszwieczorek.eu>
2023-03-03 20:04:21 +0100

    second solution

--- Directions Reduction/solution.cs
+++ Directions Reduction/solution.cs
@@ -1,22 +1,22 @@
 using System;
 using System.Linq;
-
+using System.Collections.Generic;
 public class DirReduction {
   
     public static string[] dirReduc(String[] arr) {
+      
+      var oppositeDirections = new Dictionary<string, string> {{"NORTH", "SOUTH"}, {"EAST", "WEST"}};
+
       return arr.Aggregate(Enumerable.Empty<string>(),
 						(verifiedDirections, nextDirection) => 
-                   verifyOppositeDirection(verifiedDirections.LastOrDefault(), nextDirection) 
-                                           ? verifiedDirections.SkipLast(1) 
-                                           : verifiedDirections.Append(nextDirection))
-            .ToArray();
+                           checkIfOppositeDirections(verifiedDirections.LastOrDefault(), nextDirection) ? 
+                           verifiedDirections.SkipLast(1) : 
+                           verifiedDirections.Append(nextDirection)).ToArray();
 
-      bool verifyOppositeDirection(string firstDirection, string secondDirection) =>
-	          (firstDirection == "NORTH" && secondDirection == "SOUTH") ||
-	          (firstDirection == "SOUTH" && secondDirection == "NORTH") ||
-	          (firstDirection == "EAST" && secondDirection == "WEST") ||
-	          (firstDirection == "WEST" && secondDirection == "EAST");  
-
+      bool checkIfOppositeDirections(string firstDirection, string secondDirection) =>
+			    (!String.IsNullOrWhiteSpace(firstDirection)) &&
+					oppositeDirections.Any(x => (x.Key == firstDirection && x.Value == secondDirection) ||
+												(x.Key == secondDirection && x.Value == firstDirection));
+    
     }
-  
 }
