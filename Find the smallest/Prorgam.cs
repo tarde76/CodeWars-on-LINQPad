544547af12253f864f5f024b4c5e4465efcb6183
Bartosz Wieczorek <github@bartoszwieczorek.eu>
2023-03-05 22:38:21 +0100

    first solution

---
+++ Find the smallest/Prorgam.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Find_the_smallest;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string? number = null;
+        do
+        {
+            Console.Write("Enter number: ");
+            number = Console.ReadLine();
+            if (Int32.TryParse(number, out var parsedNumber))
+            {
+                var smallest = ToSmallest.Smallest(parsedNumber);
+                Console.WriteLine($"{parsedNumber} = [{String.Join(", ", smallest)}]");
+            }
+            else if (!String.IsNullOrWhiteSpace(number))
+                Console.WriteLine("Wrong number. Try again");
+        } while (!String.IsNullOrWhiteSpace(number));
+    }
+}
