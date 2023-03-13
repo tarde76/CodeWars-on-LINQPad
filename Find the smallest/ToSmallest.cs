51f581bc59e26efa3697113a0b9db0420a84a95d
Bartosz Wieczorek <github@bartoszwieczorek.eu>
2023-03-13 14:45:25 +0100

    changed field names in local tuple

--- Find the smallest/ToSmallest.cs
+++ Find the smallest/ToSmallest.cs
@@ -8,17 +8,17 @@ public class ToSmallest
     public static long[] Smallest(long n)
     {
         var numberString = n.ToString();
-        var numbersList = new List<(long Number, long RemovalIndex, long InsertionIndex)>();
+        var numbersList = new List<(long number, long removalIndex, long insertionIndex)>();
 
         for (int i = 0; i < numberString.Length; i++)
             generateCombinationsForIndex(i);
 
-        var smallestNumber = numbersList.OrderBy(x => x.Number)
-                                        .ThenBy(x => x.RemovalIndex)
-                                        .ThenBy(x => x.InsertionIndex)
+        var smallestNumber = numbersList.OrderBy(x => x.number)
+                                        .ThenBy(x => x.removalIndex)
+                                        .ThenBy(x => x.insertionIndex)
                                         .First();
 
-        return new long[] { smallestNumber.Number, smallestNumber.RemovalIndex, smallestNumber.InsertionIndex };
+        return new long[] { smallestNumber.number, smallestNumber.removalIndex, smallestNumber.insertionIndex };
 
         void generateCombinationsForIndex(int index)
         {
@@ -28,9 +28,9 @@ public class ToSmallest
             for (int i = 0; i <= numberStringWithoutDigit.Length; i++)
             {
                 numbersList.Add((
-                    Number: Convert.ToInt64(numberStringWithoutDigit.Insert(i, Char.ToString(removedDigit))),
-                    RemovalIndex: index,
-                    InsertionIndex: i
+                    number: Convert.ToInt64(numberStringWithoutDigit.Insert(i, Char.ToString(removedDigit))),
+                    removalIndex: index,
+                    insertionIndex: i
                 ));
             }
         }
