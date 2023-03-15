b4ec20765f8e3511635e465ebb55805d516610f9
Bartosz Wieczorek <github@bartoszwieczorek.eu>
2023-03-15 19:41:56 +0100

    PyramidSlideDown: first solution

---
+++ PyramidSlideDown/PyramidSlideDown.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace PyramidSlideDown;
+public class PyramidSlideDown
+{
+    public static int LongestSlideDown(int[][] pyramid)
+    {
+        int[] results = pyramid[^1];
+
+        for (int i = pyramid.Length - 2; i >= 0; i--)
+        {
+            int[] currentRow = pyramid[i];
+
+            var currentResults = 
+                currentRow.Select((x, index) => x + Math.Max(results[index], results[index + 1]))
+                          .ToArray<int>();
+                                            
+            results = currentResults;
+        }
+
+        return results[0];
+    }
+}
