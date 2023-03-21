e7a39eed323f463e86c26b2fcc33c41e5d03c06a
Bartosz Wieczorek <github@bartoszwieczorek.eu>
2023-03-21 16:10:56 +0100

    wrong result, before changing checking algorithm

--- RectangleRotation/RectangleRotation.cs
+++ RectangleRotation/RectangleRotation.cs
@@ -4,50 +4,56 @@ public class Kata
 {
     public int RectangleRotation(int a, int b)
     {
-        int result = 0;
-        int x = 0, y = 0;
-
         //int shorterSide = Math.Min(a, b);
-        var horizontalShift = b * Math.Sqrt(2) / 2;
-        var verticalShift = a * Math.Sqrt(2) / 2;;
+        //var horizontalShift = b * Math.Sqrt(2) / 2;
+        double angleInDegrees = 45.0; 
+        double angleInRadians = angleInDegrees * (Math.PI / 180.0); 
+        double cosineValue = Math.Cos(angleInRadians); 
 
-        int stepY = 0, stepX = 0;
+        double verticalShift = b / (2 * cosineValue);
+        double horizontalShift = a / (2 * cosineValue);
+
         bool inRectangleBoundsXPlusYPlus = true, inRectangleBoundsXPlusYMinus = true,
              inRectangleBoundsXMinusYPLus = true, inRectangleBoundsXMinusYMinus = true;
+
+        int result = 0;
+        int x = 0, y = 0, stepX = 0, stepY = 0;
 
         do
         {
             do
             {
-                inRectangleBoundsXPlusYPlus = inRectangle(x + stepX, y + stepY);
-                inRectangleBoundsXPlusYMinus = stepY != 0 ? inRectangle(x + stepX, y - stepY) : false;
-                inRectangleBoundsXMinusYPLus = stepX != 0 ? inRectangle(x - stepX, y + stepY) : false;
-                inRectangleBoundsXMinusYMinus = stepY != 0 ? inRectangle(x - stepX, y - stepY) : false;
+                inRectangleBoundsXPlusYPlus = inRectangle((x + stepX, y + stepY));
+                inRectangleBoundsXPlusYMinus = stepY != 0 ? inRectangle((x, y - stepY)) : false;
+                inRectangleBoundsXMinusYPLus = x != 0 ? inRectangle((x - stepX, y)) : false;
+                inRectangleBoundsXMinusYMinus = stepY != 0 && x !=0 ? inRectangle((x - stepX, y - stepY)) : false;
 
                 stepX++;
             } while (inRectangleBoundsXPlusYPlus || inRectangleBoundsXPlusYMinus ||
                     inRectangleBoundsXMinusYPLus || inRectangleBoundsXMinusYMinus);
-            stepY++;
             stepX = 0;
+            stepY++;
+            x++; 
+            y++;
 
-            inRectangleBoundsXPlusYPlus = inRectangle(x + stepX, y + stepY);
-            inRectangleBoundsXMinusYMinus = stepY != 0 ? inRectangle(x - stepX, y - stepY) : false;
+            inRectangleBoundsXPlusYPlus = inRectangle((x, y));
+            inRectangleBoundsXMinusYMinus = stepX != 0 && stepY != 0? inRectangle((x - stepX, y - stepY)) : false;
 
         } while (inRectangleBoundsXPlusYPlus || inRectangleBoundsXMinusYMinus);
 
         return result;
 
-        bool inRectangle(int x, int y)
+        bool inRectangle((int x, int y) point)
         {
             // y = x + a    => y < x + a
             // y = -x + a   => y < -x + a
             // y = x - a    => y > x - a
             // y = -x - a   => y > -x - a
 
-            if (y <= x + horizontalShift &&        // y < x + a
-                y >= x - horizontalShift &&        // y > x - a
-                y <= -x + verticalShift  &&    // y < -x + a
-                y >= -x - verticalShift)       // y > -x - a
+            if (point.y <= point.x + horizontalShift &&        // y < x + a
+                point.y >= point.x - horizontalShift &&        // y > x - a
+                point.y <= -point.x + verticalShift  &&    // y < -x + a
+                point.y >= -point.x - verticalShift)       // y > -x - a
             {
                 result++;
                 return true;
