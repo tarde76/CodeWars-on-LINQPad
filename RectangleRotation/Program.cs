7c351a3acf4675531b83d27cb16adce583161534
Bartosz Wieczorek <github@bartoszwieczorek.eu>
2023-03-21 12:24:11 +0100

    wrong resukt, before removing stepX and stepY variables

---
+++ RectangleRotation/Program.cs
@@ -0,0 +1,14 @@
+﻿using RectangleRotation;
+using System.Collections.Generic;
+
+var tests = new List<(int a, int b)> {(a: 6, b: 4),
+                                      (a: 30, b: 2),
+                                      (a: 8, b: 6),
+                                      (a: 16, b: 20)};
+
+foreach (var test in tests)
+{
+    Console.WriteLine($"Number of points with integral coordinates a={test.a}, b={test.b} :" + 
+                      $"{new Kata().RectangleRotation(test.a, test.b)}");
+}
+
---
+++ RectangleRotation/RectangleRotation.cs
@@ -0,0 +1,58 @@
+﻿namespace RectangleRotation;
+
+public class Kata
+{
+    public int RectangleRotation(int a, int b)
+    {
+        int result = 0;
+        int x = 0, y = 0;
+
+        //int shorterSide = Math.Min(a, b);
+        var horizontalShift = b * Math.Sqrt(2) / 2;
+        var verticalShift = a * Math.Sqrt(2) / 2;;
+
+        int stepY = 0, stepX = 0;
+        bool inRectangleBoundsXPlusYPlus = true, inRectangleBoundsXPlusYMinus = true,
+             inRectangleBoundsXMinusYPLus = true, inRectangleBoundsXMinusYMinus = true;
+
+        do
+        {
+            do
+            {
+                inRectangleBoundsXPlusYPlus = inRectangle(x + stepX, y + stepY);
+                inRectangleBoundsXPlusYMinus = stepY != 0 ? inRectangle(x + stepX, y - stepY) : false;
+                inRectangleBoundsXMinusYPLus = stepX != 0 ? inRectangle(x - stepX, y + stepY) : false;
+                inRectangleBoundsXMinusYMinus = stepY != 0 ? inRectangle(x - stepX, y - stepY) : false;
+
+                stepX++;
+            } while (inRectangleBoundsXPlusYPlus || inRectangleBoundsXPlusYMinus ||
+                    inRectangleBoundsXMinusYPLus || inRectangleBoundsXMinusYMinus);
+            stepY++;
+            stepX = 0;
+
+            inRectangleBoundsXPlusYPlus = inRectangle(x + stepX, y + stepY);
+            inRectangleBoundsXMinusYMinus = stepY != 0 ? inRectangle(x - stepX, y - stepY) : false;
+
+        } while (inRectangleBoundsXPlusYPlus || inRectangleBoundsXMinusYMinus);
+
+        return result;
+
+        bool inRectangle(int x, int y)
+        {
+            // y = x + a    => y < x + a
+            // y = -x + a   => y < -x + a
+            // y = x - a    => y > x - a
+            // y = -x - a   => y > -x - a
+
+            if (y <= x + horizontalShift &&        // y < x + a
+                y >= x - horizontalShift &&        // y > x - a
+                y <= -x + verticalShift  &&    // y < -x + a
+                y >= -x - verticalShift)       // y > -x - a
+            {
+                result++;
+                return true;
+            };
+            return false;
+        }
+    }
+}
