35fffee79f28fddf9e373bfa34a9a5f1d69161b3
Bartosz Wieczorek <github@bartoszwieczorek.eu>
2023-03-29 01:31:35 +0200

    ParseInt: correct solution

---
+++ ParseInt/Program.cs
@@ -0,0 +1,16 @@
+﻿using ParseInt;
+
+var numbersToParse = new List<string>
+    {
+        "one", "twenty", "two hundred", "forty-six",
+        "two hundred forty-six",
+        "one thousand thirty",
+        "seven hundred eighty-three thousand nine hundred and nineteen",
+        "one million seven hundred eighty-three thousand nine hundred and nineteen"
+    };
+
+foreach (var numberToParse in numbersToParse)
+{
+    Console.WriteLine($"{numberToParse}: {Parser.ParseInt(numberToParse)}");
+}
+
