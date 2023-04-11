af0616007df8f175fef8960a4cd3b5c0eb88cc78
Bartosz Wieczorek <github@bartoszwieczorek.eu>
2023-04-11 17:31:32 +0200

    TotalAreaCoveredByRectangles: working, but needs optimization - total time of tests over allowed 12s

---
+++ TotalAreaCoveredByRactangles/Kata.cs
@@ -0,0 +1,45 @@
+﻿namespace TotalAreaCoveredByRectangles;
+public class Kata
+{
+    record Rectangle(int x0, int y0, int x1, int y1);
+
+    public static long Calculate(IEnumerable<int[]> rectangles)
+    {
+        var orderedRectangles = rectangles.Select(r => new Rectangle(r[0], r[1], r[2], r[3]))
+                                           .OrderBy(r => r.x0)
+                                           .ThenBy(r => r.y0)
+                                           .ThenByDescending(r => r.x1)
+                                           .ThenByDescending(r => r.y1);
+
+        long totalArea = orderedRectangles
+                                  .Select((r, index) => calculateArea(r) - 
+                                               calculateOverlappingArea(r, orderedRectangles
+                                                                  .Skip(index + 1)
+                                                                  .TakeWhile(q => q.x0 < r.x1 && q.y0 < r.y1)))
+                                  .Sum();
+
+        return totalArea;
+
+        long calculateArea(Rectangle rectangle){
+            var result = (rectangle.x1 - rectangle.x0) * (rectangle.y1 - rectangle.y0);
+            return result;
+        }
+
+        long calculateOverlappingArea(Rectangle rectangle, IEnumerable<Rectangle> overlappingRectangles){
+            if (!overlappingRectangles.Any())
+                return 0;
+
+            var overlappingPartsOfRectangles = overlappingRectangles
+                        .Select(r => new Rectangle(Math.Max(rectangle.x0, r.x0),
+                                                   Math.Max(rectangle.y0, r.y0),
+                                                   Math.Min(rectangle.x1, r.x1),
+                                                   Math.Min(rectangle.y1, r.y1)));
+            var areaOfOverlappingPartsOfRectangles = overlappingPartsOfRectangles
+                    .Select(r => calculateArea(r))
+                    .Sum();
+            
+            return areaOfOverlappingPartsOfRectangles - calculateOverlappingArea(overlappingPartsOfRectangles.First(),
+                                                                                 overlappingPartsOfRectangles.Skip(1));
+        }
+    }
+}
---
+++ TotalAreaCoveredByRactangles/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using TotalAreaCoveredByRectangles;
+
+Console.WriteLine(Kata.Calculate(new [] { new [] {0,0,1,1}}));
+
+Console.WriteLine(Kata.Calculate(new [] { new [] {0,4,11,6}}));
+Console.WriteLine(Kata.Calculate(new [] { new [] {0,0,1,1}, new [] {1,1,2,2}}));
+Console.WriteLine(Kata.Calculate((new [] { new [] {0,0,1,1}, new [] {0,0,2,2}})));
+Console.WriteLine(Kata.Calculate((new [] { new [] {3,3,8,5}, new [] {6,3,8,9}, new [] {11,6,14,12}})));
+
+
+// Console.WriteLine(Kata.Calculate(new [] { new [] {0,0,1,1}, new [] {0,4,1,6}}));
+
+// Console.WriteLine(Kata.Calculate(new [] { new [] {0,0,1,1}, new [] {0,0,2,2}}));
+
+// Console.WriteLine(Kata.Calculate(new [] { new [] {3,3,8,5}, new [] {6,3,8,9}, new [] {11,6,14,12}}));
+
+//  Console.WriteLine(Kata.Calculate(new [] { 
+//                                 new [] {1,2,4,4}
+//                                 , new [] {6,2,10,6}
+//                                 , new [] {7,3,9,5}
+//                                 }));
+
+ Console.WriteLine(Kata.Calculate(new [] {
+                                    new [] {13, 1, 17, 4},
+                                    new [] {14, 2, 16, 5},
+                                    new [] {15, 3, 21, 6}
+                                  }));
