378b6bfdd4dc97cfc4d424971d2f74d1483455d2
Bartosz Wieczorek <github@bartoszwieczorek.eu>
2023-04-14 23:02:21 +0200

    TheSupermarketQueue: optimized

--- The Supermarket Queue/Kata.cs
+++ The Supermarket Queue/Kata.cs
@@ -3,30 +3,28 @@ public class Kata
 {
     public static long QueueTime(int[] customers, int n)
     {
-        var checkoutTill = new int[n];
-        var customersQueue = new Queue<int>(customers);
+        if (customers.Length == 0)
+            return 0;
+        if (customers.Length <= n)
+            return customers.Max();
 
-        for (int i = 0; i < checkoutTill.Length; i++)
-        {
-            customersQueue.TryDequeue(out checkoutTill[i]);
-            if (customersQueue.Count == 0)
-                return checkoutTill.Max();
-        }
-
+        var checkoutTills = customers.Take(n).ToArray<int>();
+        var customersQueue = new Queue<int>(customers.Skip(n));
         int timeCounter = 0;
 
         while (customersQueue.Count != 0)
         {
-            var leastTimeToCheckout = checkoutTill.Min();
+            var leastTimeToCheckout = checkoutTills.Min();
             timeCounter += leastTimeToCheckout;
-            for (int i = 0; i < checkoutTill.Length; i++)
+
+            for (int i = 0; i < checkoutTills.Length; i++)
             {
-                checkoutTill[i] -= leastTimeToCheckout;
-                if (checkoutTill[i] == 0 )
-                    customersQueue.TryDequeue(out checkoutTill[i]);
+                checkoutTills[i] -= leastTimeToCheckout;
+                if (checkoutTills[i] == 0 )
+                    customersQueue.TryDequeue(out checkoutTills[i]);
             }
         }
 
-        return timeCounter + checkoutTill.Max();
+        return timeCounter + checkoutTills.Max();
     }
 }
--- TotalAreaCoveredByRactangles/Kata.cs
+++ TotalAreaCoveredByRactangles/Kata.cs
@@ -1,39 +1,31 @@
 ﻿namespace TotalAreaCoveredByRectangles;
 public class Kata
 {
-    record Rectangle(int x0, int y0, int x1, int y1);
-
     public static long Calculate(IEnumerable<int[]> rectangles)
     {
-        var orderedRectangles = rectangles.Select(r => new Rectangle(r[0], r[1], r[2], r[3]));
-                                        //    .OrderBy(r => r.x0)
-                                        //    .ThenBy(r => r.y0)
-                                        //    .ThenByDescending(r => r.x1)
-                                        //    .ThenByDescending(r => r.y1);
 
-        long totalArea = orderedRectangles
+        long totalArea = rectangles
                                   .Select((r, index) => calculateArea(r) - 
-                                               calculateOverlappingArea(r, orderedRectangles
+                                               calculateOverlappingArea(r, rectangles
                                                                   .Skip(index + 1)
-                                                                  .TakeWhile(q => q.x0 < r.x1 && q.y0 < r.y1)))
+                                                                  .TakeWhile(q => q[0] < r[2] && q[1] < r[3])))
                                   .Sum();
 
         return totalArea;
 
-        long calculateArea(Rectangle rectangle){
-            var result = (rectangle.x1 - rectangle.x0) * (rectangle.y1 - rectangle.y0);
-            return result;
+        long calculateArea(int[] rectangle){
+            return (rectangle[2] - rectangle[0]) * (rectangle[3] - rectangle[1]);
         }
 
-        long calculateOverlappingArea(Rectangle rectangle, IEnumerable<Rectangle> overlappingRectangles){
+        long calculateOverlappingArea(int[] rectangle, IEnumerable<int[]> overlappingRectangles){
             if (!overlappingRectangles.Any())
                 return 0;
 
             var overlappingPartsOfRectangles = overlappingRectangles
-                        .Select(r => new Rectangle(Math.Max(rectangle.x0, r.x0),
-                                                   Math.Max(rectangle.y0, r.y0),
-                                                   Math.Min(rectangle.x1, r.x1),
-                                                   Math.Min(rectangle.y1, r.y1)));
+                        .Select(r => new int [] {Math.Max(rectangle[0], r[0]),
+                                                 Math.Max(rectangle[1], r[1]),
+                                                 Math.Min(rectangle[2], r[2]),
+                                                 Math.Min(rectangle[3], r[3])});
             var areaOfOverlappingPartsOfRectangles = overlappingPartsOfRectangles
                     .Select(r => calculateArea(r))
                     .Sum();
